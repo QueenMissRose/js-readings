7021df2320b212f2d8cce287eac8e0288e64cbfc
QueenMissRose <107987783+QueenMissRose@users.noreply.github.com>
2024-11-20 23:54:40 -0500

    first commit

---
+++ js-info-function-basics/js/main.js
@@ -0,0 +1,31 @@
+// function checkAge(age) {
+//   return (age > 18) || confirm('did you get parents permission?')
+// }
+
+// console.log(checkAge(19))
+
+
+// function min(a,b) {
+//   if (a < b) {
+//     return a;
+//   } else {
+//     return b;
+//   }
+// }
+
+// console.log(min(2,5))
+
+// function min(a,b) {
+//   return (a < b) ? a : b;
+// }
+
+// console.log(min(5,1))
+
+
+function pow(x,n) {
+  x = prompt('please enter a number')
+  n = prompt('please enter the exponent')
+  alert(x ** n)
+}
+
+pow()

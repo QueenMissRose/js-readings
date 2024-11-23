fd0daf407e34abfd0722b7ca53b2ec13960b3d8e
QueenMissRose <107987783+QueenMissRose@users.noreply.github.com>
2024-11-22 23:08:02 -0500

    started function basics tasks

--- js-info-function-basics/js/main.js
+++ js-info-function-basics/js/main.js
@@ -1,31 +1,29 @@
-// function checkAge(age) {
-//   return (age > 18) || confirm('did you get parents permission?')
-// }
-
-// console.log(checkAge(19))
-
-
-// function min(a,b) {
-//   if (a < b) {
-//     return a;
-//   } else {
-//     return b;
-//   }
-// }
-
-// console.log(min(2,5))
-
-// function min(a,b) {
-//   return (a < b) ? a : b;
-// }
-
-// console.log(min(5,1))
-
-
-function pow(x,n) {
-  x = prompt('please enter a number')
-  n = prompt('please enter the exponent')
-  alert(x ** n)
+// TASK 1
+// is 'else' required?
+/*
+The following function returns true if the parameter age is greater than 18.
+Otherwise it asks for a confirmation and returns its result:
+*/
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  } else {
+    // ...
+    return confirm('Did parents allow you?');
+  }
 }
-
-pow()
+/*
+Will the function work differently if else is removed? 
+my ans: NO
+*/
+function checkAge(age) {
+  if (age > 18) {
+    return true;
+  }
+  // ...
+  return confirm('Did parents allow you?');
+}
+/*
+Is there any difference in the behavior of these two variants?
+my ans: no difference
+*/

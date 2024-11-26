e58de87ef5a4fc0b8b46fbefb896ed9ef4f8b866
QueenMissRose <107987783+QueenMissRose@users.noreply.github.com>
2024-11-25 23:30:18 -0500

    js loops task 2 and 3

--- thejsway-loops/js/main.js
+++ thejsway-loops/js/main.js
@@ -28,21 +28,102 @@
 //* TASK 2
 //* Parity
 // Check the following program that shows even numbers (divisible by 2) between 1 and 100
-for (let i = 1; i  <= 10; i++) {
-  if ( i % 2 === 0){
-    console.log(`${i} is even`)
-  }
-}
+// for (let i = 1; i  <= 10; i++) {
+//   if ( i % 2 === 0){
+//     console.log(`${i} is even`)
+//   }
+// }
 // This program uses the modulo operator %, which calculates the remainder after division of one number by another. It's often used to assess number parity.
-console.log(10 % 2); // 0 because 10 = 5 * 2 + 0
-console.log(11 % 2); // 1 because 11 = 5 * 2 + 1
-console.log(18 % 3); // 0 because 18 = 3 * 6 + 0
-console.log(19 % 3); // 1 because 19 = 3 * 6 + 1
-console.log(20 % 3); // 2 because 20 = 3 * 6 + 2
+// console.log(10 % 2); // 0 because 10 = 5 * 2 + 0
+// console.log(11 % 2); // 1 because 11 = 5 * 2 + 1
+// console.log(18 % 3); // 0 because 18 = 3 * 6 + 0
+// console.log(19 % 3); // 1 because 19 = 3 * 6 + 1
+// console.log(20 % 3); // 2 because 20 = 3 * 6 + 2
 // Improve the program so that it also shows odd numbers.
 
+//? while loop
+// let num = 0
+// while (num <= 100){
+//   if(num % 2 === 0){
+//     console.log(`${num} is even`)
+//   } else {
+//     console.log(`${num} is odd`)
+//   }
+//   num++
+// }
+
+//? for loop
+// for (i = 0; i <= 100; i++){
+//   if(i % 2 === 0){
+//     console.log(`${i} is even`)
+//   }else {
+//     console.log(`${i} is odd`)
+//   }
+// }
 
 // Improve it again to replace the initial number 1 by a number given by the user.
-
 //!info
 // This program must show exactly 10 numbers including the first one, not 11 numbers!
+//? while loop
+// const startingNum = Number(prompt('Input a starting number'))
+// let num = startingNum
+// while(num < startingNum + 10){
+//   if(num % 2 == 0){
+//     console.log(`${num} is even`)
+//   }else{
+//     console.log(`${num} is odd`)
+//   }
+//   num++
+// }
+
+//? for loop
+// const startingNum = Number(prompt('Input a starting number'))
+// for(i = startingNum; i < startingNum + 10; i++){
+//   if (i % 2 == 0){
+//     console.log(`${i} is even`)
+//   }else{
+//     console.log(`${i} is odd`)
+//   }
+// }
+
+//* TASK 3
+//* Input validation
+// Write a program that continues to ask the user for a number until the entered number is less than or equal to 100.
+//? while loop
+// num = Number(prompt('Input a number less than or equal to 100'))
+// while(num > 100){
+//   alert(`${num} is not less than or equal to 100, try again`)
+//   num = Number(prompt('Input a number less than or equal to 100'))
+// }
+
+//? for loop ~ I can only do it with a function
+// function lessThan100(){
+//   let inputNumber = Number(prompt('Input a number less than or equal to 100'))
+//   for(num = inputNumber; num > 100; lessThan100()){
+//     alert(`${num} is not less than or equal to 100`)
+//   };
+// }
+// lessThan100()
+
+// when you are done with the above, improve the program so that the terminating number is between 50 and 100.
+//? while
+// //peusdo code 
+// // ask user for number
+// let num = Number(prompt('please input a number between 50 and 100'))
+// // calculate if number is between 50 - 100
+// while(num > 100 || num < 50){
+//   alert(`${num} is not between 50 and 100`)
+//   // if NOT ask for another number
+//   num = Number(prompt('please input a number between 50 and 100'))
+// }
+// // if yes, end loop
+
+//? for (not quite working how i want...)
+function between50And100(){
+  let inputNumber = Number(prompt('Please input a number between 50 and 100'))
+  for(num = inputNumber; num > 100 || num < 50; between50And100()){
+    alert(`${num} is not between 50 and 100`)
+    num = Number(prompt('Please input a number between 50 and 100'))
+  }
+}
+between50And100()
